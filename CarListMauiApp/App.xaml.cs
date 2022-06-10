7c9e8938655d4a865dd79a585e3c0cb40b13b3b5
Sonia <soniagoodluck@yahoo.co.uk>
2022-06-10 18:51:34 +0100

    Add project files.

---
+++ CarListMauiApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CarListMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CarListMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CarListMauiApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
